63dd90afd0638237ecd93860d053bb4f6485bc61
tomaszpro99 <tomaszprostko99@gmail.com>
2023-08-30 03:16:45 +0200

    backup

--- src/main/java/io/github/tomaszpro99/model/SqlTaskRepository.java
+++ src/main/java/io/github/tomaszpro99/model/SqlTaskRepository.java
@@ -1,32 +1,24 @@
-package io.github.tomaszpro99.model; // TaskRepository - klasa ktora sluzy do komunikacji z baza danych
+package io.github.tomaszpro99.adapter; // TaskRepository - klasa ktora sluzy do komunikacji z baza danych
 //api - punkt wejscia do dzialania na zbiorze
 
-import  org.springframework.data.jpa.repository.JpaRepository;
+import io.github.tomaszpro99.model.Task;
+import io.github.tomaszpro99.model.TaskRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-//public class TaskRepository {
-//@RepositoryRestResource(path = "todos",collectionResourceRel = "todos")
-@RepositoryRestResource //chcemy zeby Spring wszedl tutaj do tego pliku i go rozpoznal // tworzymy repozytorium i caly interface Rest'owy - czyli udostepnimy na zewnatrz wszystkie metody, http, tworzenia, aktualizowania
+//@RepositoryRestResource //chcemy zeby Spring wszedl tutaj do tego pliku i go rozpoznal // tworzymy repozytorium i caly interface Rest'owy - czyli udostepnimy na zewnatrz wszystkie metody, http, tworzenia, aktualizowania
+@Repository
 interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> { //Jpa - stad Sql
-//nie mamy tu dostepu ze wzgledu na pakietowy dostep
-
-    //uniemozliwamiy usuniecie taska //ukrywamy metody //406 Method Not Allowed
-    @Override
-    @RestResource(exported = false)
-    void deleteById(Integer integer); //usuwa po id
 
     @Override
-    @RestResource(exported = false)
-    void delete(Task task); //usuwa encje
+    @Query(nativeQuery = true, value = "select count(*) > 0 from tasks where id=:id")
+    boolean existsById(@Param("id") Integer id);
 
-    @RestResource(path = "done", rel = "done") //zmiana nazwy adr -> path-w adr url ,rel-w laczach
-    //List<Task> findByDoneIsTrue(); //metoda zwracajaca liste<Taskow> //znajdz wszystkie taski ustawione na true  //zmieniamy na uniwersalna t/f
-    List<Task> findByDone(@Param("state")boolean done); //Nie podalismy wartosci dla flagi Done, to musi zostac przekazana jako parametr boolean
-    //dzieki temu mozemy sobie wyszukiwac localhost:8080/tasks/search/done?state=false
-
+    /*@Override
+    boolean existsByDoneIsFalseAndGroup_Id(Integer groupId);*/
 }
 
--- src/main/java/io/github/tomaszpro99/model/TaskGroup.java
+++ src/main/java/io/github/tomaszpro99/model/TaskGroup.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Table(name = "task_groups")
@@ -16,10 +17,18 @@ public class TaskGroup {
     private boolean done;
     @Embedded
     private Audit audit = new Audit();
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "group")
+    private Set<Task> tasks;
     public TaskGroup() {
     }
     public int getId() { return id; }    public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
     public boolean isDone() { return done; }
     public void setDone(boolean done) { this.done = done; }
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+    public void setTasks(Set<Task> tasks) {
+        this.tasks = tasks;
+    }
 }
